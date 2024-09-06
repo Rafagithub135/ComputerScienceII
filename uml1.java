8c9221b88c9f5ddb4c81bb2d02d177be46bbe38e
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-09-05 21:16:25 -0400

    started working on uml list

---
+++ uml1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class uml1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count;
+        String employeeID;
+        String lastName;
+        String firstName;
+        double rate;
+        double hours;
+        Boolean certified;
+    }
+
+
+}
