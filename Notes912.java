4c82e5cb869de99aa37cdef041a574e943479d7d
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2024-09-12 15:15:21 -0400

    commented out notes

--- Notes912.java
+++ Notes912.java
@@ -1,11 +1,11 @@
-public class Notes912 {
-    public static void main(String[] args) {
-        private int value;
-        private String suit;
-
-        public card(Int value, String suit) {
-            this.value = value;
-            this.suit = suit;
-        }
-    }
-}
+//public class Notes912 {
+//    public static void main(String[] args) {
+//        private int value;
+//        private String suit;
+//
+//        public card(Int value, String suit) {
+//            this.value = value;
+//            this.suit = suit;
+//        }
+//    }
+//}
