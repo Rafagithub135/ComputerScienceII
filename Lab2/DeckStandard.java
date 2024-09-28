7b05df117f86dd411c39c22283a1368023a2767c
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-09-28 13:59:56 -0400

    working on lab2

---
+++ Lab2/DeckStandard.java
@@ -0,0 +1,20 @@
+package Lab2;
+
+import java.util.Random;
+
+public class DeckStandard {
+    public static void main(String[] args) {
+        Deck d = new Deck();
+        d.shuffle();
+
+        Card[] hand = new Card[1];
+
+        for (int i = 0; i < 1; i++) {
+            hand[i] = d.dealCard();
+        }
+
+        for (Card c : hand) {
+            System.out.println(c);
+        }
+    }
+}
