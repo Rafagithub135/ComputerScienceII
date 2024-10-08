26b08dd2ecce82b9207dcc062d62f55930af5949
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-10-07 23:25:37 -0400

    working on lab 3

---
+++ Lab3/FindItem.java
@@ -0,0 +1,15 @@
+package Lab3;
+
+import java.util.Random;
+
+public class FindItem {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[] terms = new int[10];
+        for (int i = 0; i < terms.length; i++) {
+            terms[i] = r.nextInt(100);
+        }
+
+
+    }
+}
