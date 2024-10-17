bb2984c90947361a6302a9115cf7a0ceb6ba4470
Ralph M. <102826377+Rafagithub135@users.noreply.github.com>
2024-10-17 16:02:13 -0400

    working on exam 1

---
+++ Exam1/DVD.java
@@ -0,0 +1,26 @@
+package Exam1;
+
+public class DVD extends AudioRecording{
+    public static void main(String[] args) {
+
+    }
+    @Override
+    public String toString() {
+        return "DVD{name, length, size}";
+    }
+
+    @Override
+    public void getFileSize(double size) {
+
+    }
+
+    @Override
+    public void songLength(double length) {
+
+    }
+
+    @Override
+    public void songName(String name) {
+
+    }
+}
