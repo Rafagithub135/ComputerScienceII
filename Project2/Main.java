b26f49a44efa5905b2e6f03b87532a93fdb299ab
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2024-10-24 13:31:54 -0400

    added file reader file

---
+++ Project2/Main.java
@@ -0,0 +1,12 @@
+package Project2;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] data = new String[2];
+
+    }
+}
