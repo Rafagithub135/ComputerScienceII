c2c89ecb6c4072b9c0cb355409ff2008bad1b997
Ralph McCloskey <mccloskey.ralph35@gmail.com>
2024-11-21 12:03:37 -0500

    added testing class

---
+++ TDD.java
@@ -0,0 +1,10 @@
+public class TDD {
+    public static void main(String[] args) {
+
+    }
+
+    public static double convertTempFToC(double degF) {
+        return (degF - 32) * (5.0 / 9.0);
+    }
+
+}
---
+++ TDDTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TDDTest {
+    @Test
+    void test() {
+
+    }
+
+
+}
