4e1a9c0d55e2ee1aa2f662c1e66e966b7f0ca39b
rafagithub135 <mccloskey.ralph35@gmail.com>
2026-03-18 10:21:38 -0400

    Working on Lab 6.

--- HackLabs/Lab6/OhmsLaw.java
+++ HackLabs/Lab6/OhmsLaw.java
@@ -12,24 +12,71 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-/**
- * Demonstrates using a JFrame to create a basic window.
- */
 public class OhmsLaw extends JFrame implements ActionListener {
 
     private JTextField amps;
     private JTextField volts;
     private JTextField ohms;
 
+
+public OhmsLaw(String OhmsLaw) {
+        super("Ohm's Law Calculator");
+        super.setSize(400, 200);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+
+        JPanel topPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+        topPanel.setLayout(new GridLayout(3,1));
+
+        JLabel label = new JLabel("Enter the current (amps): ");
+        amps = new JTextField(15);
+        JLabel label2 = new JLabel("Enter the voltage (volts): ");
+        volts = new JTextField(15);
+        JLabel label3 = new JLabel("Enter the resistance (ohms): ");
+        ohms = new JTextField(15);
+
+        JPanel topRow = new JPanel();
+        JPanel rowTwoPanel = new JPanel();
+        JPanel rowThreePanel = new JPanel();
+
+        topPanel.add(amps);
+        topPanel.add(amps);
+        topPanel.add(volts);
+        topPanel.add(volts);
+        topPanel.add(ohms);
+        topPanel.add(ohms);
+
+        topPanel.add(topRow);
+        topPanel.add(rowTwoPanel);
+        topPanel.add(rowThreePanel);
+
+        JButton calculateButton = new JButton("Calculate");
+
+        calculateButton.setActionCommand("Calculate");
+
+        calculateButton.addActionListener(this);
+
+        bottomPanel.add(calculateButton);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+        super.add(mainPanel);
+        super.setVisible(true);
+}
+
+public void actionPerformed(ActionEvent e) {
+    if (e.getActionCommand().equals("Calculate")) {
+        if (amps.getText().equals("") || volts.getText().equals("") || ohms.getText().equals("")) {
+            
+        }
+    }
+}
+
     public static void main(String[] args) {
         MultiplePanelWindow w = new MultiplePanelWindow("Ohm's Law Calculator");
     }
-
-public MultiplePaneWindow(String, "Ohm's Law'");
-    JLabel amps = new JLabel("Enter the current (amps):");                                                                 //Creates the Labels and Text Fields
-    field = new JTextField(15);
-    JLabel volts = new JLabel("Enter the voltage (volts):");
-    field2 = new JTextField(15);
-    JLabel ohms = new JLabel("Enter the resistance (ohms):");
-    ohms = new JTextField(15);
 }
